ff263db02b7beb5305f18e8f57965cae748d9fd8
donald <809077529@qq.com>
2017-06-09 14:55:15 +0800

    update

---
+++ app/src/main/java/com/fanhl/androidgroovy/MainActivity2.java
@@ -0,0 +1,8 @@
+package com.fanhl.androidgroovy;
+
+import android.support.v7.app.ActionBarActivity;
+
+public class MainActivity2 extends ActionBarActivity {
+
+
+}
---
+++ app/src/test/groovy/com/fanhl/androidgroovy/Detail.groovy
@@ -0,0 +1,6 @@
+package com.fanhl.androidgroovy;
+
+public trait Detail {
+    String name
+    String value
+}
